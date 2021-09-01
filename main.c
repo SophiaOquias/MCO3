c9ce2dd74e5744f5453f55782d943d8689a33d9a
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-09-01 11:39:31 +0800

    tested main algorithm

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include "bst.h"
 
 int main() {
-	nodeType *tree = create("first"); 
+	nodeType *tree = NULL; // initialize tree
 	string words[10] = {"apple", "banana", "banana", "orange", "cherry", "umbrella", "penguin", "orange", "nine", "ten"}; 
 	int i;
 	
 	for(i = 0; i < 10; i++) {
-		if(search(tree, words[i]) == 1) {
+		if(tree == NULL) {
+			tree = create(words[i]); 
+		}
+		else if(search(tree, words[i]) == 1) {
 			incrementCount(searchNode(tree, words[i]));
 		}
 		else
