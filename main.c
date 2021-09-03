8edab62e55e3518d5aaf31dcab3c2db060feb47e
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-09-03 12:36:11 +0800

    editted main.c to read files

--- main.c
+++ main.c
@@ -1,28 +1,18 @@
-#include "bst.h"
+#include "fileRead.h"
 
 int main() {
 	nodeType *tree = NULL; // initialize tree
-	string words[10] = {"apple", "banana", "banana", "orange", "cherry", "umbrella", "penguin", "orange", "nine", "ten"}; 
-	int i;
+	FILE *fp; 
 	
-	for(i = 0; i < 10; i++) {
-		if(tree == NULL) {
-			tree = create(words[i]); 
-		}
-		else if(search(tree, words[i]) == 1) {
-			incrementCount(searchNode(tree, words[i]));
-		}
-		else
-			insert(tree, words[i]); 
-	}
+	fp = fopen("input.txt", "r"); 
 	
-	inorder(tree); 
-	
-	printf("------------------------------------------------------------\n");
-	
-	destroy(tree);
+	tree = readFile(fp, tree); 
 	
 	inorder(tree);
+
+	destroy(tree); 
+	
+	fclose(fp); 
 	
 	return 0; 
 }
