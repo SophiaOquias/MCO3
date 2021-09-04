5f4eec8d3035e00896a420f595ef9a209ac12339
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-09-04 09:53:12 +0800

    added main algorithm

--- main.c
+++ main.c
@@ -2,17 +2,36 @@
 
 int main() {
 	nodeType *tree = NULL; // initialize tree
+	string filename; 
 	FILE *fp; 
+	char temp; 
 	
-	fp = fopen("input.txt", "r"); 
+	do {
+		fprintf(stdout, "enter filename (<filename>.txt): ");
+		fscanf(stdin, "%s", filename); 
+		fscanf(stdin, "%c", &temp); // to ignore the enter key 
+		
+		if((fp = fopen(filename, "r")) == NULL)
+			fprintf(stderr, "<%s> not found\n", filename);
+			
+		fprintf(stdout, "\n\n");
+			
+	} while((fp = fopen(filename, "r")) == NULL);
 	
 	tree = readFile(fp, tree); 
 	
 	inorder(tree);
-
-	destroy(tree); 
+	
+	fclose(fp);
+	
+	fp = fopen("WORDS.txt", "w"); 
+	
+	outputFile(fp, tree);
+	fprintf(stdout, "\n\nOutput file WORDS.txt successfully created"); 
 	
 	fclose(fp); 
+
+	destroy(tree);  
 	
 	return 0; 
 }
