2a8f01faa351f6e259e6a241a4f2282c06a6a360
Sophia Oquias <sophia_oquias@dlsu.edu.ph>
2021-09-05 10:12:31 +0800

    added openFile function and removed part that converts whole world to lowercase

--- fileRead.h
+++ fileRead.h
@@ -1,47 +1,56 @@
 #include "bst.h"
 
+// opens input textfile and displays error message if file not found 
+FILE *openFile (){
+	FILE *fp;
+	char fname[100], temp;
+  
+	do {
+    	printf ("Enter filename: ");
+    	scanf("%s", fname);
+		scanf("%c", &temp); // to accept 
+    	fp = fopen(fname, "r");
+
+		if (fp == NULL)
+      		printf("<%s> not found\n\n", fname);
+
+  	} while (fp == NULL);
+  
+  return fp;
+}
+
+/* reads text file and stores words in a bst
+returns the root node */
 nodeType *readFile(FILE *fp, nodeType *root) {
 	string temp = "";
 	char c; 
-	int i = 0;
-	
-	// while there are still characters to read in the text file 
+	int i = 0; 
+
 	while((fscanf(fp, "%c", &c)) == 1) {
-		
+
 		if(isLetter(c) && c != '\n') {
+			if(isCapital(c))
+				c = convertLowCase(c); 
 			temp[i] = c; 
 			i++; 
 		}
 		else {
 			temp[i] = '\0'; 
-			
-			convertLowCase(temp); 
-			
-			if(strlen(temp) >= 3) { // check if word is not less than 3 letters in length 
-				if(root == NULL) {
-					root = create(temp); 
-				}
-				else if (search(root, temp) == 1) {  // increment count of word if already exists in tree
-					incrementCount(searchNode(root, temp)); 
-				}
-				else { // insert new node if not found
-					insert(root, temp); 
-				}
-			}
-			
-			i = 0; 
-			strcpy(temp, ""); 
-		}
 
-	} 
-	
+			if(strlen(temp) >= 3) {
+				if(root == NULL) 
+					root = create(temp); // create node if tree is empty
+				else if(search(root, temp) == 1)
+					incrementCount(searchNode(root, temp)); // increment count if word already exists in tree
+				else
+					insert(root, temp); // insert new node if word does not already exist in tree 
+			}
+
+			i = 0; 
+			strcpy(temp, ""); // reinitialize temp to null string
+		}
+	}
+
 	return root; 
 }
 
-void outputFile(FILE *fp, nodeType *root) {
-	if(root != NULL) {
-		outputFile(fp, root->left);
-		fprintf(fp, "%-15s\t:\t%d\n", root->key, root->count);
-		outputFile(fp, root->right);
-	}
-}
